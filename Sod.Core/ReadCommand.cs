ab290bf314da5402ec66c77753f043275c2fec6b
Artur Rachwal <artur.rachwal@maersk.com>
2021-10-10 15:55:05 +0200

    frame calculator and state mapping added

---
+++ Sod.Core/ReadCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Sod.Core
+{
+    public enum ReadCommand
+    {
+        ZonesViolation = 0x00,
+        ArmedPartitionsSuppressed = 0x09,
+        OutputsState = 0x17,
+        Troubles = 0x1C
+    }
+}
---
+++ Sod.Core/UpdateCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace Sod.Core
+{
+    public enum UpdateCommand
+    {
+        OutputsSwitch = 0x91
+    }
+}
