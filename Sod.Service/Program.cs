50279792777a2a335fa90ea0d57834894271e6b5
artur rachwal <arturrachwal@gmail.com>
2021-11-03 22:26:40 +0100

    worker service added

---
+++ Sod.Service/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Sod.Service
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) => { services.AddHostedService<Worker>(); });
+    }
+}
