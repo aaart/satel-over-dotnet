c621a012d1e908a92ae06c955ecff33f57481119
artur rachwal <arturrachwal@gmail.com>
2021-11-16 22:24:36 +0100

    previous commit supplement

--- Sod.Infrastructure/State/Loop/StepType/ReadInputs.cs
+++ Sod.Infrastructure/State/Loop/StepType/ReadInputs.cs
@@ -1,13 +1,23 @@
-﻿using Sod.Infrastructure.Satel;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Sod.Infrastructure.Satel;
+using Sod.Infrastructure.State.Events;
 using Sod.Infrastructure.Store;
 
 namespace Sod.Infrastructure.State.Loop.StepType
 {
-    public class ReadInputs : ReadOutputs
+    public class ReadInputs : ReadState
     {
-        public ReadInputs(IStore store, IManipulator manipulator) 
-            : base(store, manipulator)
+        public ReadInputs(IStore store, IManipulator manipulator, IEventPublisher eventPublisher) 
+            : base(store, manipulator, eventPublisher)
         {
+            
         }
+
+        protected override string PersistedStateKey => Constants.Store.InputsStateKey;
+
+        protected override Task<(CommandStatus, bool[])> ManipulatorMethod() => Manipulator.ReadInputs();
+        
+        protected override void Notify(IEnumerable<int> changedStates) => EventPublisher.Publish(new Event(EventType.InputsStateChanged, changedStates));
     }
 }
--- Sod.Infrastructure/State/Loop/StepType/ReadOutputs.cs
+++ Sod.Infrastructure/State/Loop/StepType/ReadOutputs.cs
@@ -1,19 +1,23 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Sod.Infrastructure.Satel;
+using Sod.Infrastructure.State.Events;
 using Sod.Infrastructure.Store;
 
 namespace Sod.Infrastructure.State.Loop.StepType
 {
-    public class ReadOutputs : BaseStep
+    public class ReadOutputs : ReadState
     {
-        public ReadOutputs(IStore store, IManipulator manipulator) 
-            : base(store, manipulator)
+        public ReadOutputs(IStore store, IManipulator manipulator, IEventPublisher eventPublisher) 
+            : base(store, manipulator, eventPublisher)
         {
+            
         }
 
-        public override Task ExecuteAsync()
-        {
-            throw new System.NotImplementedException();
-        }
+        protected override string PersistedStateKey => Constants.Store.OutputsStateKey;
+        
+        protected override Task<(CommandStatus, bool[])> ManipulatorMethod() => Manipulator.ReadOutputs();
+
+        protected override void Notify(IEnumerable<int> changedStates) => EventPublisher.Publish(new Event(EventType.OutputsStateChanged, changedStates));
     }
 }
