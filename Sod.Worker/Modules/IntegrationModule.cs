ead5a57fb1c44a0d398101a386a37bafbf1aa01d
artur rachwal <artur.rachwal@gmail.com>
2021-12-07 22:32:18 +0100

    integration module added

---
+++ Sod.Worker/Modules/IntegrationModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Sod.Infrastructure.Satel;
+using Sod.Infrastructure.Satel.Communication;
+using Sod.Infrastructure.Satel.State.Events;
+using Sod.Infrastructure.Satel.State.Events.Mqtt;
+using Sod.Infrastructure.Satel.State.Loop;
+
+namespace Sod.Worker.Modules
+{
+    public class IntegrationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterType<Manipulator>().As<IManipulator>().SingleInstance();
+            builder.RegisterType<StepCollectionFactory>().As<IStepCollectionFactory>().SingleInstance();
+            builder.RegisterType<MqttOutgoingChangeNotifier>().As<IOutgoingChangeNotifier>().SingleInstance();
+        }
+    }
+}
