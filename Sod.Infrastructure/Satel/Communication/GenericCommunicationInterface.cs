4985fbc58a05ed97107584ba31c14e9cf7ea4146
aaart <artur.rachwal@gmail.com>
2022-02-17 19:17:39 +0100

    Monitor removed

--- Sod.Infrastructure/Satel/Communication/GenericCommunicationInterface.cs
+++ Sod.Infrastructure/Satel/Communication/GenericCommunicationInterface.cs
@@ -8,8 +8,6 @@ namespace Sod.Infrastructure.Satel.Communication
 {
     public class GenericCommunicationInterface
     {
-        private static readonly object _lock = new object();
-        
         private readonly ISocketReceiver _socketReceiver;
         private readonly ISocketSender _socketSender;
 
@@ -23,27 +21,17 @@ namespace Sod.Infrastructure.Satel.Communication
 
         public async Task<(CommandStatus status, TResp)> Execute<TResp>(CommunicationMessage message, CommunicationDefaultResponse<TResp> defaultResponse, Func<byte[], TResp> resultTranslation)
         {
-            bool sent;
-            try
-            {
-                Monitor.Enter(_lock);
-                sent = await SendAsync(_socketSender, message.Command, message.NewState, message.UserCode);
-            }
-            finally
-            {
-                Monitor.Exit(_lock);
-            }
-            
+            var sent = await SendAsync(_socketSender, message.Command, message.NewState, message.UserCode);
             if (!sent)
             {
                 return (CommandStatus.NotSent, defaultResponse.Value);
             }
 
             var (status, data) = await ReceiveAsync(_socketReceiver, defaultResponse.ExpectedCommand);
-            return 
-                status != CommandStatus.Processed 
-                ? (status, defaultResponse.Value) 
-                : (CommandStatus.Processed, resultTranslation(data));
+            return
+                status != CommandStatus.Processed
+                    ? (status, defaultResponse.Value)
+                    : (CommandStatus.Processed, resultTranslation(data));
         }
     }
 }
