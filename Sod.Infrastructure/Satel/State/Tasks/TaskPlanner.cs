f2b92518a75038d0957a2d28e53e4ffa42a656a6
aaart <artur.rachwal@gmail.com>
2022-02-17 18:55:06 +0100

    exception handling

--- Sod.Infrastructure/Satel/State/Tasks/TaskPlanner.cs
+++ Sod.Infrastructure/Satel/State/Tasks/TaskPlanner.cs
@@ -1,33 +1,44 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Sod.Infrastructure.Capabilities;
 using Sod.Infrastructure.Enums;
 using Sod.Infrastructure.Storage;
 using Task = System.Threading.Tasks.Task;
 
 namespace Sod.Infrastructure.Satel.State.Tasks
 {
-    public class TaskPlanner : ITaskPlanner
+    public class TaskPlanner : LoggingCapability, ITaskPlanner
     {
         private readonly int _maxIterationCount;
-        private int _iteration = 0;
+        private int _iteration;
 
         public TaskPlanner(int maxIterationCount)
         {
             _maxIterationCount = maxIterationCount;
         }
-        
+
         public Task Plan(ITaskQueue queue)
         {
-            if (_iteration == 0)
+            try
             {
-                queue.Enqueue(new SatelTask(TaskType.ReadOutputs));
-                queue.Enqueue(new SatelTask(TaskType.ReadInputs));
-            }
+                if (_iteration == 0)
+                {
+                    queue.Enqueue(new SatelTask(TaskType.ReadOutputs));
+                    queue.Enqueue(new SatelTask(TaskType.ReadInputs));
+                }
 
-            if (++_iteration == _maxIterationCount)
+                if (++_iteration == _maxIterationCount)
+                {
+                    _iteration = 0;
+                }
+            }
+            catch (Exception e)
             {
+                Logger.LogError(e, e.Message);
                 _iteration = 0;
             }
-            
+
             return Task.CompletedTask;
         }
     }
