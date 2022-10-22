9cfd74187c342c90660c625c96d3ec827da68a4b
aaart <artur.rachwal@gmail.com>
2022-10-22 23:00:00 +0200

    commit

--- Sod.Model/Tasks/Handlers/BaseHandler.cs
+++ Sod.Model/Tasks/Handlers/BaseHandler.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Sod.Infrastructure.Capabilities;
 
-namespace Sod.Model.Tasks.Handlers
+namespace Sod.Model.Tasks.Handlers;
+
+public abstract class BaseHandler<T> : LoggingCapability, ITaskHandler
+    where T : SatelTask
 {
-    public abstract class BaseHandler<T> : LoggingCapability, ITaskHandler
-        where T : SatelTask
+        
+    public async Task<IEnumerable<SatelTask>> Handle(SatelTask data)
     {
-        
-        public async Task<IEnumerable<SatelTask>> Handle(SatelTask data)
-        {
-            data = data ?? throw new ArgumentNullException(nameof(data));
-            return await Handle((T)data);
-        }
-        
-        protected abstract Task<IEnumerable<SatelTask>> Handle(T data);
+        data = data ?? throw new ArgumentNullException(nameof(data));
+        return await Handle((T)data);
     }
+        
+    protected abstract Task<IEnumerable<SatelTask>> Handle(T data);
 }
