8af77ba8ad55f4f923f0b3e84c6488b7e539b747
aaart <artur.rachwal@gmail.com>
2022-11-26 22:43:35 +0100

    commit

--- Sod.Model/Tasks/Handlers/BaseHandler.cs
+++ Sod.Model/Tasks/Handlers/BaseHandler.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Sod.Infrastructure.Capabilities;
+﻿using Sod.Infrastructure.Capabilities;
 
 namespace Sod.Model.Tasks.Handlers;
 
