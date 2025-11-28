b09a634aabb29fb6c43c87b3ae2a431e2720bf6b
Artur Rachwal <artur.rachwal@gmail.com>
2025-11-28 23:23:55 +0100

    more primary conistructors

--- Sod.Infrastructure/Satel/Communication/GenericCommunicationInterface.cs
+++ Sod.Infrastructure/Satel/Communication/GenericCommunicationInterface.cs
@@ -7,30 +7,22 @@ using static Sod.Infrastructure.Satel.Communication.Communication;
 
 namespace Sod.Infrastructure.Satel.Communication;
 
-public class GenericCommunicationInterface : LoggingCapability
+public class GenericCommunicationInterface(
+    ISocketReceiver socketReceiver,
+    ISocketSender socketSender)
+    : LoggingCapability
 {
-    private readonly ISocketReceiver _socketReceiver;
-    private readonly ISocketSender _socketSender;
-
-    public GenericCommunicationInterface(
-        ISocketReceiver socketReceiver,
-        ISocketSender socketSender)
-    {
-        _socketReceiver = socketReceiver;
-        _socketSender = socketSender;
-    }
-
     public async Task<(CommandStatus status, TResp)> Execute<TResp>(CommunicationMessage message, CommunicationDefaultResponse<TResp> defaultResponse, Func<byte[], TResp> resultTranslation)
     {
         Logger.LogDebug($"Executing {message.Command.ToString()} command.");
-        var sent = await SendAsync(_socketSender, message.Command, message.NewState, message.UserCode);
+        var sent = await SendAsync(socketSender, message.Command, message.NewState, message.UserCode);
         if (!sent)
         {
             Logger.LogDebug("Command not send. Exiting.");
             return (CommandStatus.NotSent, defaultResponse.Value);
         }
 
-        var (status, data) = await ReceiveAsync(_socketReceiver, defaultResponse.ExpectedCommand);
+        var (status, data) = await ReceiveAsync(socketReceiver, defaultResponse.ExpectedCommand);
         Logger.LogDebug($"{message.Command.ToString()} executed with {status.ToString()} status.");
         if (status != CommandStatus.Processed)
         {
